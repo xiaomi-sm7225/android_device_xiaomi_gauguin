d4bb27a979079e1e2639bc2549d16bdd0d7e367e
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:09:15 -0400

    gauguin: Use Clang r563880c for kernel build
    * Doesn't boot with default Clang version.
    
    Change-Id: I35765e296044eef428d01bbf5a21e91f1897704e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig vendor/xiaomi/gauguin.config
 TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/gauguin
+TARGET_KERNEL_CLANG_VERSION := r563880c
 
 # Platform
 TARGET_BOARD_PLATFORM := lito
